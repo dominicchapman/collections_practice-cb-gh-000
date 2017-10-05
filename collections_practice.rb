73607faae355e73659657f757523883738cc6524
Dominic Chapman <dominicchapman@me.com>
2017-10-05 22:45:26 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,3 +10,11 @@ end
 def sort_array_char_count(array)
   array.sort_by { |x| x.length }
 end
+
+def swap_elements(array)
+  # swap second and third elements, i.e. index 1 and 2
+  swapped = array
+  swapped[2] = array[1]
+  swapped[1] = array[2]
+  return swapped
+end
